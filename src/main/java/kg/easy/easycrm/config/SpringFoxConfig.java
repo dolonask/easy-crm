89d72ac2c36a4924596a4130496882a3a202dffd
daskarbekov@megacom.local <daskarbekov@megacom.kg>
2021-07-08 14:36:13 +0600

    swagger

--- src/main/java/kg/easy/easycrm/config/SpringFoxConfig.java
+++ src/main/java/kg/easy/easycrm/config/SpringFoxConfig.java
@@ -1,12 +1,16 @@
 package kg.easy.easycrm.config;
 
+import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -14,9 +18,15 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("app.megacom.kg:9090/test-app")
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
